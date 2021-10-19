8c3133b841ebaf7ba46796ac2985c4204d98c19e
Seogeurim <geulims@naver.com>
2021-10-19 18:29:04 +0900

    [feat] update user router example
    - add user repository & service
    - add example code of find user by id

--- src/api/routes/users/users.controller.ts
+++ src/api/routes/users/users.controller.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import { Container } from 'typedi';
+import UserService from '../../../services/user';
 
-export const handleUserTest = (_req: Request, res: Response, next: NextFunction) => {
+export const handleUserTest = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    res.status(200).json('users router test');
+    const userServiceInstance = Container.get(UserService);
+    const user = await userServiceInstance.getUser(1);
+
+    res.status(200).json(user);
   } catch (e) {
     next(e);
   }
