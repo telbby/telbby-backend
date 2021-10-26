882d0c66f6d30e1831b008f624d6987744b682e5
Seogeurim <geulims@naver.com>
2021-10-26 20:31:13 +0900

    fix: validation error 처리 수정
    validation error가 적절히 나오도록 코드 수정

--- src/api/validation/user.ts
+++ src/api/validation/user.ts
@@ -1,28 +1,23 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
-import { commonError } from '../../constants/error';
 import ErrorResponse from '../../utils/error-response';
 
 export const userTestValidation = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const schema = Joi.object({
-      id: Joi.number().required().messages({
-        'any.required': `아이디를 입력해주세요`,
-      }),
+  const schema = Joi.object({
+    id: Joi.number().required().messages({
+      'any.required': `아이디를 입력해주세요`,
+    }),
+  });
+
+  const validationResult = schema.validate(req.query);
+
+  if (validationResult.error) {
+    throw new ErrorResponse({
+      statusCode: 400,
+      message: validationResult.error.message,
     });
-
-    const validationResult = schema.validate(req.query);
-
-    if (validationResult.error) {
-      throw new ErrorResponse({
-        statusCode: 400,
-        message: validationResult.error.message,
-      });
-    }
-
-    next();
-  } catch (e) {
-    throw new ErrorResponse(commonError.wrong);
   }
+
+  next();
 };
