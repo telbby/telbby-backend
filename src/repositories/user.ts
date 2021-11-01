ac7d67a1cbbc71c970640d5064a4e0c3b6d882ad
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-01 23:02:09 +0900

    [#4] feat: user repository에 findByIdx 메서드 추가

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -4,7 +4,12 @@ import UserEntity from '../entity/user';
 
 @EntityRepository(UserEntity)
 class UserRepository extends Repository<UserEntity> {
-  async findById(id: number): Promise<UserEntity | undefined> {
+  async findByIdx(idx: number): Promise<UserEntity | undefined> {
+    const user = await this.findOne({ where: { idx } });
+    return user;
+  }
+
+  async findById(id: string): Promise<UserEntity | undefined> {
     const user = await this.findOne({ where: { id } });
     return user;
   }
