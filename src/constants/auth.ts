796ab61c0c003d2a2f63362c039be5a49db9012e
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-02 10:22:22 +0900

    [#4] refactor: password max 길이를 35자로 수정

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -4,4 +4,4 @@ export const USER_ID_MIN_LENGTH = 3;
 export const USER_ID_MAX_LENGTH = 30;
 
 export const USER_PW_MIN_LENGTH = 4;
-export const USER_PW_MAX_LENGTH = 20;
+export const USER_PW_MAX_LENGTH = 35;
