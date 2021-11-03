964254d3190128c52abf4da7922b29ba23257acf
Yunseo Hwang <yunseo.dev@gmail.com>
2021-11-03 20:24:32 +0900

    [#4] refactor: Raw Error 객체에 대한 처리 로직을 service 클래스들로부터 삭제
    Raw Error 객체에 대한 처리를 error middleware에서 해주고 있어서 불필요하다고 판단되어 삭제했습니다.

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -21,50 +21,34 @@ class AuthService {
   }
 
   async login(id: string, password: string): Promise<{ access: string; refresh: string }> {
-    try {
-      const user = await this.userRepository.findById(id);
-      if (!user) {
-        throw new ErrorResponse(commonError.unauthorized);
-      }
-
-      const isValid = comparePassword(user.password, password);
-      if (!isValid) {
-        throw new ErrorResponse(commonError.unauthorized);
-      }
-
-      const tokens = this.jwtHelper.generateJwtTokens(user);
-      return tokens;
-    } catch (e) {
-      if ((e as ErrorResponse)?.isOperational) {
-        throw e;
-      }
-      console.error(e);
-      throw new ErrorResponse(commonError.wrong);
+    const user = await this.userRepository.findById(id);
+    if (!user) {
+      throw new ErrorResponse(commonError.unauthorized);
     }
+
+    const isValid = comparePassword(user.password, password);
+    if (!isValid) {
+      throw new ErrorResponse(commonError.unauthorized);
+    }
+
+    const tokens = this.jwtHelper.generateJwtTokens(user);
+    return tokens;
   }
 
   async refreshAccessToken(refreshToken: string): Promise<{ access: string }> {
-    try {
-      if (!refreshToken) {
-        throw new ErrorResponse(commonError.unauthorized);
-      }
-
-      const { idx } = this.jwtHelper.decodeRefreshToken(refreshToken);
-
-      const user = await this.userRepository.findByIdx(idx);
-      if (!user) {
-        throw new ErrorResponse(commonError.unauthorized);
-      }
-
-      const access = this.jwtHelper.generateAccessToken(user);
-      return { access };
-    } catch (e) {
-      if ((e as ErrorResponse)?.isOperational) {
-        throw e;
-      }
-      console.error(e);
-      throw new ErrorResponse(commonError.wrong);
+    if (!refreshToken) {
+      throw new ErrorResponse(commonError.unauthorized);
     }
+
+    const { idx } = this.jwtHelper.decodeRefreshToken(refreshToken);
+
+    const user = await this.userRepository.findByIdx(idx);
+    if (!user) {
+      throw new ErrorResponse(commonError.unauthorized);
+    }
+
+    const access = this.jwtHelper.generateAccessToken(user);
+    return { access };
   }
 }
 
--- src/services/user.ts
+++ src/services/user.ts
@@ -16,39 +16,24 @@ class UserService {
   }
 
   async getUser(idx: number): Promise<{ id: string } & UpdateInfo> {
-    try {
-      const user = await this.userRepository.findByIdx(idx);
-      if (!user) {
-        throw new ErrorResponse(commonError.unauthorized);
-      }
-      const { id, createdAt, updatedAt } = user;
-      return { id, createdAt, updatedAt };
-    } catch (e) {
-      if ((e as ErrorResponse)?.isOperational) {
-        throw e;
-      }
-      throw new ErrorResponse(commonError.wrong);
+    const user = await this.userRepository.findByIdx(idx);
+    if (!user) {
+      throw new ErrorResponse(commonError.unauthorized);
     }
+    const { id, createdAt, updatedAt } = user;
+    return { id, createdAt, updatedAt };
   }
 
   async createUser(id: string, password: string): Promise<{ idx: number } & UpdateInfo> {
-    try {
-      const alreadyRegisteredUser = await this.userRepository.findById(id);
-      if (alreadyRegisteredUser) {
-        throw new ErrorResponse(commonError.conflict);
-      }
-
-      const hashedPassword = generateHash(password);
-      const createdUser = await this.userRepository.addItem(id, hashedPassword);
-      const { idx, createdAt, updatedAt } = createdUser;
-      return { idx, createdAt, updatedAt };
-    } catch (e) {
-      if ((e as ErrorResponse)?.isOperational) {
-        throw e;
-      }
-      console.error(e);
-      throw new ErrorResponse(commonError.wrong);
+    const alreadyRegisteredUser = await this.userRepository.findById(id);
+    if (alreadyRegisteredUser) {
+      throw new ErrorResponse(commonError.conflict);
     }
+
+    const hashedPassword = generateHash(password);
+    const createdUser = await this.userRepository.addItem(id, hashedPassword);
+    const { idx, createdAt, updatedAt } = createdUser;
+    return { idx, createdAt, updatedAt };
   }
 }
 
