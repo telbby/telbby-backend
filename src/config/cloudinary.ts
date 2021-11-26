abbcbcaa808aceaa1ea11a4de4d6c69aa45a49ba
박종호 <jong951005@gmail.com>
2021-11-26 23:58:26 +0900

    [#14] refactor: v2 식별자만 import 하도록 수정

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,11 +1,7 @@
-import cloudinaryModule from 'cloudinary';
+import { v2 } from 'cloudinary';
 
-const cloudinary = cloudinaryModule.v2;
-
-cloudinary.config({
+export default v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-export default cloudinary;
