9ed0212b738f2e7f42eecc681814a999343ebb39
Seogeurim <geulims@naver.com>
2021-12-13 01:57:35 +0900

    [#22] fix: 회원가입 validator 수정
    - util 함수에서 정규식 및 validator 함수 수정
    - test 코드 테스트 케이스 추가

--- tests/utils/validation.test.ts
+++ tests/utils/validation.test.ts
@@ -6,17 +6,26 @@ describe('validation utils 테스트', () => {
       expect(idValidator('')).toBe(false);
     });
     it(`4자보다 길이가 짧을 경우 거짓을 반환해야 합니다.`, () => {
-      expect(idValidator('1')).toBe(false);
+      expect(idValidator('a')).toBe(false);
+      expect(idValidator('aaa')).toBe(false);
     });
     it(`30자보다 길이가 길 경우 거짓을 반환해야 합니다.`, () => {
       const MAX_LENGTH = 30;
-      expect(idValidator('1'.repeat(MAX_LENGTH + 1))).toBe(false);
+      expect(idValidator('a'.repeat(MAX_LENGTH + 1))).toBe(false);
+    });
+    it(`4자일 경우 참을 반환해야 합니다.`, () => {
+      expect(idValidator('asdf')).toBe(true);
+    });
+    it(`30자일 경우 참을 반환해야 합니다.`, () => {
+      const MAX_LENGTH = 30;
+      expect(idValidator('a'.repeat(MAX_LENGTH))).toBe(true);
     });
     it(`특수문자가 포함될 경우 거짓을 반환해야 합니다.`, () => {
       expect(idValidator('ABCabc!@#')).toBe(false);
     });
     it(`숫자로 시작할 경우 거짓을 반환해야 합니다.`, () => {
       expect(idValidator('123ABC')).toBe(false);
+      expect(idValidator('1asd')).toBe(false);
     });
     it('4 ~ 30자 사이의 영문자로 시작하는 영대소문자 + 숫자 조합은 참을 반환해야 합니다.', () => {
       expect(idValidator('ABCabc123')).toBe(true);
@@ -35,11 +44,20 @@ describe('validation utils 테스트', () => {
       expect(pwValidator('')).toBe(false);
     });
     it(`10자보다 길이가 짧을 경우 거짓을 반환해야 합니다.`, () => {
-      expect(pwValidator('1')).toBe(false);
+      expect(pwValidator('a1')).toBe(false);
+      expect(pwValidator('bbb111bbb')).toBe(false);
+      expect(pwValidator('12345678a')).toBe(false);
     });
     it(`35자보다 길이가 길 경우 거짓을 반환해야 합니다.`, () => {
       const MAX_LENGTH = 35;
-      expect(pwValidator('1'.repeat(MAX_LENGTH + 1))).toBe(false);
+      expect(pwValidator('1'.repeat(MAX_LENGTH) + 'a')).toBe(false);
+    });
+    it(`10자일 경우 참을 반환해야 합니다.`, () => {
+      expect(pwValidator('123456789a')).toBe(true);
+    });
+    it(`35자일 경우 참을 반환해야 합니다.`, () => {
+      const MAX_LENGTH = 35;
+      expect(pwValidator('1'.repeat(MAX_LENGTH - 1) + 'a')).toBe(true);
     });
     it('10 ~ 35자 사이의 영대문자 + 영소문자 조합은 참을 반환해야 합니다. ', () => {
       expect(pwValidator('ABCabcABCa')).toBe(true);
